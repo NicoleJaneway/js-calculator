5717bcd75959e3054a92c9b0f6e4c58aabd807e2
Nicole Janeway Bills <nicolejanewaybills@gmail.com>
2021-11-14 20:09:15 -0600

    update

--- dist/script.js
+++ dist/script.js
@@ -1,4 +1,8 @@
-import React, { useState, useEffect, useRef } from "https://cdn.skypack.dev/react";
+import React, {
+useState,
+useEffect,
+useRef } from
+"https://cdn.skypack.dev/react";
 import ReactDOM from "https://cdn.skypack.dev/react-dom";
 
 function App() {
@@ -17,6 +21,7 @@ function App() {
   -  Handles 5 decimal places of precision with rouding
   */
 
+  / * DEBUGGING * /;
   useEffect(() => {
     console.log({ numberGroup });
   }, [numberGroup]);
@@ -25,44 +30,57 @@ function App() {
     console.log({ expression });
   }, [expression]);
 
+  /* Make sure two operators aren't used in succession */
   useEffect(() => {
     if (/\s[\*\/\+\-]\s\s[\*\/\+\-]\s$/.test(expression)) {
       console.log("Use of double operators");
       const newestOperator = expression.match(/\s[\*\/\+\-]\s$/); // grab last operator
       console.log(newestOperator);
-      setExpression(prev => prev.split("").slice(0, prev.length - 6).join("") + newestOperator);
+      setExpression(
+      (prev) =>
+      prev.
+      split("").
+      slice(0, prev.length - 6).
+      join("") + newestOperator);
+
     } else {
       setDisplay(expression);
     }
   }, [expression]);
 
   const handleNumber = symbol => {
-    if (answer !== "") {// if a number is selected and answer is not blank, clear answer
+    if (answer !== "") {
+      // if a number is selected and answer is not blank, clear answer
       setAnswer("");
       setExpression("");
+      setShowApproxSign(false);
     }
     setNumberGroup(prev => prev + symbol);
     setExpression(prev => prev + symbol);
   };
 
   const handleZero = symbol => {
-    if (/^0/.test(numberGroup)) {// if a number group starts with a zero, 
+    if (/^0/.test(numberGroup)) {
+      // if a number group starts with a zero,
       // make sure it isn't immediately followed by another zero
       // do nothing
     } else {
       setNumberGroup(prev => prev + symbol);
       setExpression(prev => prev + symbol);
+      setShowApproxSign(false);
     }
   };
 
   const handleDecimal = symbol => {
     if (numberGroup === "") {
       setNumberGroup("0.");
-    } else if (/\./.test(numberGroup)) {// make sure number group can only contain one zero
-      // do nothing      
+    } else if (/\./.test(numberGroup)) {
+      // make sure number group can only contain one zero
+      // do nothing
     } else {
       setNumberGroup(prev => prev + symbol);
       setExpression(prev => prev + symbol);
+      setShowApproxSign(false);
     }
   };
 
@@ -82,6 +100,7 @@ function App() {
     } else if (/\s=/.test(expression)) {
       setExpression(answer + symbol);
       setAnswer("");
+      setShowApproxSign(false);
     } else {
       setExpression(prev => prev + symbol);
       setNumberGroup("");
@@ -91,9 +110,7 @@ function App() {
   const calculate = () => {
     if (expression.length > 0) {
       let value = String(eval(expression));
-      if (/\./.test(value)) {
-        console.log("Count: ", value.match(/(?:\.\d*)/)[0].length);
-        if (value.match(/(?:\.\d*)/)[0] > 6)
+      if (value.match(/(?:\.\d*)/)[0].length > 6) {
         console.log("long");
         value = String(eval(expression).toPrecision(5));
         setShowApproxSign(true);
@@ -107,45 +124,159 @@ function App() {
   const allClear = () => {
     setExpression("");
     setAnswer("");
+    setShowApproxSign(false);
   };
 
   const clear = () => {
-    setExpression(prev => prev.split("").slice(0, prev.length - 1).join(""));
+    setExpression((prev) =>
+    prev.
+    split("").
+    slice(0, prev.length - 1).
+    join(""));
+
     setAnswer("");
+    setShowApproxSign(false);
   };
 
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/
-  React.createElement("div", { className: "container" }, /*#__PURE__*/
-  React.createElement("div", { className: "grid" }, /*#__PURE__*/
-  React.createElement("div", { id: "display" }, /*#__PURE__*/
-  React.createElement("input", { type: "text", value: display, placeholder: "0", disabled: true }), /*#__PURE__*/
-  React.createElement("div", { style: { display: "flex", justifyContent: "space-between" } },
-  showApproxSign ? /*#__PURE__*/React.createElement("div", { className: "approx-sign" }, "~") : /*#__PURE__*/React.createElement("div", { className: "no-approx-sign" }), /*#__PURE__*/
-  React.createElement("div", { className: "total" }, answer === "" ? /*#__PURE__*/React.createElement("span", null, "\xA0\xA0") : answer))), /*#__PURE__*/
+  return /*#__PURE__*/(
+    React.createElement(React.Fragment, null, /*#__PURE__*/
+    React.createElement("div", { className: "container" }, /*#__PURE__*/
+    React.createElement("div", { className: "grid" }, /*#__PURE__*/
+    React.createElement("div", { id: "display" }, /*#__PURE__*/
+    React.createElement("input", { type: "text", value: display, placeholder: "0", disabled: true }), /*#__PURE__*/
+    React.createElement("div", { style: { display: "flex", justifyContent: "space-between" } },
+    showApproxSign ? /*#__PURE__*/
+    React.createElement("div", { className: "approx-sign" }, "~") : /*#__PURE__*/
+
+    React.createElement("div", { className: "no-approx-sign" }), /*#__PURE__*/
+
+    React.createElement("div", { className: "total" },
+    answer === "" ? /*#__PURE__*/React.createElement("span", null, "\xA0\xA0") : answer))), /*#__PURE__*/
 
 
-  React.createElement("div", { onClick: allClear, className: "padButton tomato", id: "clear" }, "AC"), /*#__PURE__*/
-  React.createElement("div", { onClick: clear, className: "padButton tomato", id: "backspace" }, "C"), /*#__PURE__*/
-  React.createElement("div", { onClick: () => handleOperator(" / "), className: "padButton", id: "divide" }, "/"), /*#__PURE__*/
-  React.createElement("div", { onClick: () => handleOperator(" * "), className: "padButton", id: "multiply" }, "x"), /*#__PURE__*/
-  React.createElement("div", { onClick: () => handleNumber("7"), className: "padButton dark-gray", id: "seven" }, "7"), /*#__PURE__*/
-  React.createElement("div", { onClick: () => handleNumber("8"), className: "padButton dark-gray", id: "eight" }, "8"), /*#__PURE__*/
-  React.createElement("div", { onClick: () => handleNumber("9"), className: "padButton dark-gray", id: "nine" }, "9"), /*#__PURE__*/
-  React.createElement("div", { onClick: () => handleMinus(" - "), className: "padButton", id: "subtract" }, "-"), /*#__PURE__*/
-  React.createElement("div", { onClick: () => handleNumber("4"), className: "padButton dark-gray", id: "four" }, "4"), /*#__PURE__*/
-  React.createElement("div", { onClick: () => handleNumber("5"), className: "padButton dark-gray", id: "five" }, "5"), /*#__PURE__*/
-  React.createElement("div", { onClick: () => handleNumber("6"), className: "padButton dark-gray", id: "six" }, "6"), /*#__PURE__*/
-  React.createElement("div", { onClick: () => handleOperator(" + "), className: "padButton", id: "add" }, "+"), /*#__PURE__*/
-  React.createElement("div", { onClick: () => handleNumber("1"), className: "padButton dark-gray", id: "one" }, "1"), /*#__PURE__*/
-  React.createElement("div", { onClick: () => handleNumber("2"), className: "padButton dark-gray", id: "two" }, "2"), /*#__PURE__*/
-  React.createElement("div", { onClick: () => handleNumber("3"), className: "padButton dark-gray", id: "three" }, "3"), /*#__PURE__*/
-  React.createElement("div", { onClick: calculate, className: "padButton custom-blue", id: "equals" }, "="), /*#__PURE__*/
-  React.createElement("div", { onClick: () => handleZero("0"), className: "padButton dark-gray", id: "zero" }, "0"), /*#__PURE__*/
-  React.createElement("div", { onClick: () => handleDecimal("."), className: "padButton dark-gray", id: "decimal" }, "."))));
+
+    React.createElement("div", { onClick: allClear, className: "padButton tomato", id: "clear" }, "AC"), /*#__PURE__*/
+
+
+    React.createElement("div", { onClick: clear, className: "padButton tomato", id: "backspace" }, "C"), /*#__PURE__*/
+
+
+    React.createElement("div", {
+      onClick: () => handleOperator(" / "),
+      className: "padButton",
+      id: "divide" }, "/"), /*#__PURE__*/
+
+
+
+    React.createElement("div", {
+      onClick: () => handleOperator(" * "),
+      className: "padButton",
+      id: "multiply" }, "x"), /*#__PURE__*/
+
+
+
+    React.createElement("div", {
+      onClick: () => handleNumber("7"),
+      className: "padButton dark-gray",
+      id: "seven" }, "7"), /*#__PURE__*/
+
+
+
+    React.createElement("div", {
+      onClick: () => handleNumber("8"),
+      className: "padButton dark-gray",
+      id: "eight" }, "8"), /*#__PURE__*/
+
+
+
+    React.createElement("div", {
+      onClick: () => handleNumber("9"),
+      className: "padButton dark-gray",
+      id: "nine" }, "9"), /*#__PURE__*/
+
+
+
+    React.createElement("div", {
+      onClick: () => handleMinus(" - "),
+      className: "padButton",
+      id: "subtract" }, "-"), /*#__PURE__*/
+
+
+
+    React.createElement("div", {
+      onClick: () => handleNumber("4"),
+      className: "padButton dark-gray",
+      id: "four" }, "4"), /*#__PURE__*/
+
+
+
+    React.createElement("div", {
+      onClick: () => handleNumber("5"),
+      className: "padButton dark-gray",
+      id: "five" }, "5"), /*#__PURE__*/
+
+
+
+    React.createElement("div", {
+      onClick: () => handleNumber("6"),
+      className: "padButton dark-gray",
+      id: "six" }, "6"), /*#__PURE__*/
+
+
+
+    React.createElement("div", {
+      onClick: () => handleOperator(" + "),
+      className: "padButton",
+      id: "add" }, "+"), /*#__PURE__*/
+
+
+
+    React.createElement("div", {
+      onClick: () => handleNumber("1"),
+      className: "padButton dark-gray",
+      id: "one" }, "1"), /*#__PURE__*/
+
+
+
+    React.createElement("div", {
+      onClick: () => handleNumber("2"),
+      className: "padButton dark-gray",
+      id: "two" }, "2"), /*#__PURE__*/
+
+
+
+    React.createElement("div", {
+      onClick: () => handleNumber("3"),
+      className: "padButton dark-gray",
+      id: "three" }, "3"), /*#__PURE__*/
+
+
+
+    React.createElement("div", {
+      onClick: calculate,
+      className: "padButton custom-blue",
+      id: "equals" }, "="), /*#__PURE__*/
+
+
+
+    React.createElement("div", {
+      onClick: () => handleZero("0"),
+      className: "padButton dark-gray",
+      id: "zero" }, "0"), /*#__PURE__*/
+
+
+
+    React.createElement("div", {
+      onClick: () => handleDecimal("."),
+      className: "padButton dark-gray",
+      id: "decimal" }, ".")))));
+
+
+
+
 
 
 
 }
 
-
-ReactDOM.render( /*#__PURE__*/React.createElement(App, null), document.getElementById('root'));
+ReactDOM.render( /*#__PURE__*/React.createElement(App, null), document.getElementById("root"));
